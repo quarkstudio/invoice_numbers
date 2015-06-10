c65c02179a5e58fb927864e11473bf86c8f0fbb6
Romain Lalaut <romain@quarkstudio.fr>
2015-06-10 11:02:02 +0200

    rails 4 support

--- lib/invoice_numbers/active_record/invoice_number_sequence.rb
+++ lib/invoice_numbers/active_record/invoice_number_sequence.rb
@@ -1,5 +1,5 @@
 class InvoiceNumberSequence < ActiveRecord::Base
-  attr_accessible :name, :next_number
+
   def self.by_sequence_name( sequence_name )
     InvoiceNumberSequence.where(name: sequence_name).first_or_create!
   end
